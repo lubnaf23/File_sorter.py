1c130ef8282c41735b5d06bc794eb9c740f7bd84
lubnaf23 <157326176+lubnaf23@users.noreply.github.com>
2024-05-08 22:18:28 -0700

    Update fileMover.py

--- fileMover.py
+++ fileMover.py
@@ -3,18 +3,16 @@ import shutil #to move files
 
 def file_mover():
 #The folder we want to clean
-#"C:\Users\syedh\Downloads"
-#"C:\Users\syedh\OneDrive\Documents"
-    cleaning_folder = '/Users/syedh/OneDrive/Documents/'
+    cleaning_folder = '/Users/name/OneDrive/Documents/' #example use local path
 
 #folder to move the file types to
 #"C:\Users\syedh\OneDrive\Pictures"
-    new_folder = '/Users/syedh/OneDrive/Pictures/'
+    new_folder = '/Users/name/OneDrive/Pictures/' #example use local path
 
 #iterating over the entire cleaning folder
     for filename in os.listdir(cleaning_folder):
     #is it a specific type of file?
-        if filename.endswith('.jpg'):
+        if filename.endswith('.jpg'): #any file type
         #existing path
             source_path = os.path.join(cleaning_folder, filename)
         #new path
